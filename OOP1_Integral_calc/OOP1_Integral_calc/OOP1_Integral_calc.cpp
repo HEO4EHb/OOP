a22c2d3b7508191c8d8f5f0be75838193cc65813
Yan <yan.matveyev@gmail.com>
2023-12-28 19:28:25 +0200

    step now depends on numPoint, upper and lower bounds

--- OOP1_Integral_calc/OOP1_Integral_calc/OOP1_Integral_calc.cpp
+++ OOP1_Integral_calc/OOP1_Integral_calc/OOP1_Integral_calc.cpp
@@ -6,7 +6,9 @@
 // Базовый класс для вычисления интегралов
 class IntegralCalc {
 public:
-    IntegralCalc(int numPoints, double step) {
+    IntegralCalc(int numPoints, double lowerBound, double upperBound) {
+        double step=(upperBound-lowerBound)/(numPoints-1);
+        std::cout << step << std::endl;
         // Проверка корректности входных данных
         if (numPoints < 2 || step <= 0.0)
             throw std::invalid_argument("Incorrect input");
@@ -27,7 +29,7 @@ protected:
 // Класс для вычисления интеграла методом трапеций
 class TrapezoidalIntegrator : public IntegralCalc {
 public:
-    TrapezoidalIntegrator(int numPoints, double step) : IntegralCalc(numPoints, step) {}
+    TrapezoidalIntegrator(int numPoints, double lowerBound, double upperBound) : IntegralCalc(numPoints, lowerBound, upperBound) {}
 
     // Переопределение метода для вычисления интеграла
     double Calc(const std::function<double(double)>& integralFunc, double lowerBound, double upperBound) const override {
@@ -50,7 +52,7 @@ public:
 // Класс для вычисления интеграла методом Симпсона
 class SimpsonIntegrator : public IntegralCalc {
 public:
-    SimpsonIntegrator(int numPoints, double step) : IntegralCalc(numPoints, step) {
+    SimpsonIntegrator(int numPoints, double lowerBound, double upperBound) : IntegralCalc(numPoints, lowerBound, upperBound) {
         // Проверка, что количество точек кратно двум (требование метода Симпсона)
         if (numPoints % 2 != 0)
             throw std::invalid_argument("Incorrect input: the number of points must be a multiple of two");
@@ -87,13 +89,13 @@ int main() {
         double upperBound = 1.0;
 
         // Интегрирование функции x^2 методом трапеций
-        TrapezoidalIntegrator trapezoidal(numPoints, step);
+        TrapezoidalIntegrator trapezoidal(numPoints, lowerBound, upperBound);
         auto integralFunc = [](double x) { return x * x; };
         double integralResult = trapezoidal.Calc(integralFunc, lowerBound, upperBound);
         std::cout << "Trapezoidal: " << integralResult << std::endl;
 
         // Интегрирование функции x^2 методом Симпсона
-        SimpsonIntegrator simpson(numPoints, step);
+        SimpsonIntegrator simpson(numPoints, lowerBound, upperBound);
         integralResult = simpson.Calc(integralFunc, lowerBound, upperBound);
         std::cout << "Simpson: " << integralResult << std::endl;
     }
